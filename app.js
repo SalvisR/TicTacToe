f5beb80010b7b62801c84dc519babda86b7e8bd4
Salvis Reimanis <salvis64@inbox.lv>
2019-12-27 17:25:58 +0200

    Draw function

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const fieldsArr = [
   [null, null, null],
   [null, null, null],
   [null, null, null]
-]
+];
 const lines = document.querySelectorAll('.line');
 let click = 0;
 let symbol = 'x';
+let msg = '';
 
 const refreshBoard = () => {
   lines.forEach((line, index) => {
@@ -14,30 +15,37 @@ const refreshBoard = () => {
       field.textContent = fieldsArr[index][i];
     });
   });
-}
+};
 refreshBoard();
 
-const checkWinner = (line) => {
+const checkWinner = line => {
   const lineLength = line.filter(f => f !== null).length;
   const unique = [...new Set(line)];
 
   if (lineLength === 3 && unique.length === 1) {
-    const div = document.createElement('div');
-    const h1 = document.createElement('h1');
-    div.classList.add('winner');
-    h1.textContent = `Winner is ${unique[0]}`;
-
-    div.appendChild(h1);
-    document.querySelector('body').appendChild(div);
-
+    msg = `Winner is ${unique[0]}`;
+    createWinnerScreen();
+  } else if (gameOver()) {
+    msg = 'Game Over';
+    createWinnerScreen();
   }
-}
+};
+
+const createWinnerScreen = () => {
+  const div = document.createElement('div');
+  const h1 = document.createElement('h1');
+  div.classList.add('winner');
+  h1.textContent = msg;
+
+  div.appendChild(h1);
+  document.querySelector('body').appendChild(div);
+};
 
 const checkHorizontal = () => {
   fieldsArr.forEach(line => {
     checkWinner(line);
   });
-}
+};
 
 const checkVertical = () => {
   for (let i = 0; i < 3; i++) {
@@ -47,7 +55,7 @@ const checkVertical = () => {
     }
     checkWinner(line);
   }
-}
+};
 
 const diagonalOne = () => {
   const line = [];
@@ -55,7 +63,7 @@ const diagonalOne = () => {
     line.push(fieldsArr[i][i]);
   }
   checkWinner(line);
-}
+};
 
 const diagonalTwo = () => {
   const lines = [];
@@ -64,14 +72,29 @@ const diagonalTwo = () => {
     lines.push(line[lineIndex]);
   });
   checkWinner(lines);
-}
+};
+
+const gameOver = () => {
+  const lines = [];
+  fieldsArr.forEach(line => {
+    line = line.filter(f => f !== null);
+    if (line.length >= 3) {
+      lines.push(line);
+    }
+  });
+
+  if (lines.length === 3) {
+    return true;
+  }
+  return false;
+};
 
 const getWinner = () => {
   checkHorizontal();
   checkVertical();
   diagonalOne();
   diagonalTwo();
-}
+};
 
 lines.forEach((line, index) => {
   line.addEventListener('click', e => {
@@ -87,7 +110,7 @@ lines.forEach((line, index) => {
       click++;
       refreshBoard();
       getWinner();
+      gameOver();
     }
-
   });
 });
